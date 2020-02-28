c81f7dccf00ccda109575d474f3b6c0cb523ba6c
aaronreulkhoo <aaronkhoo@live.com>
2020-02-28 12:01:32 +0800

    wrap-up for first git commit

--- server.js
+++ server.js
@@ -7,24 +7,60 @@ const app = express();
 const uri = "mongodb+srv://aaron:aaron@esc-mongo-4dgm3.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true });
 
-// serve simple html
-app.use(express.static('public'))
-
-//middleware
-app.use(bodyParser.json());
-
-// route setup
-app.use('/api', require('./routes/api'));
-
-//error handling
-app.use(function(err,req,res,next){
+app.use(express.static('public')); // serve simple html
+app.use(bodyParser.json()); //middleware
+app.use('/api', require('./routes/api')); // route setup
+app.use(function(err,req,res,next){ //error handling
     console.log(err.message);
     res.status(422).send({error: err.message})
 });
 
-// listening
+// api
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
 
+/*-----------------------Rainbow API SDK*/
+// Load the SDK
+let RainbowSDK = require("rainbow-node-sdk");
+
+// Define your configuration
+let options = {
+    "rainbow": {
+        "host": "sandbox",
+    },
+    "credentials": {
+        "login": "bot@mycompany.com",
+        "password": "thePassword!123"
+    },
+    "application": {
+        "appID": "",
+        "appSecret": "",
+    },
+    // Logs options
+    "logs": {
+        "enableConsoleLogs": true,            // Default: true
+        "enableFileLogs": false,              // Default: false
+        "file": {
+            "path": '/var/tmp/rainbowsdk/',   // Default path used
+            "level": 'debug'                  // Default log level used
+        }
+    },
+    ProxyImpl
+    "proxy": {
+        "host": '192.168.0.254',
+        "port": 8080,
+        "protocol": 'http',
+        "user": "",
+        "password": ""
+    },
+    // IM options
+    "im": {
+        "sendReadReceipt": true   // True to send the the 'read' receipt automatically
+    }
+};
+
+// Instantiate the SDK
+let rainbowSDK = new RainbowSDK(options);
+rainbowSDK.start();
