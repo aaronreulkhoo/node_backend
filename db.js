d54246232edb372a52a43814c1ed7a02decddd03
Genius-wangzilin <57471983+Genius-wangzilin@users.noreply.github.com>
2020-03-14 23:26:21 +0800

    Add options

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // setting up mongo
 const uri = "mongodb+srv://aaron:aaron@esc-mongo-4dgm3.mongodb.net/test?retryWrites=true&w=majority";
 async function connect(){
-    mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connect(uri,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
     console.log("Connected to Atlas Cluster!")
 }
 
