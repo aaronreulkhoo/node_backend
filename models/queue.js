8188e1b63afa191258d5330f4112269725cd985d
Genius-wangzilin <57471983+Genius-wangzilin@users.noreply.github.com>
2020-03-14 23:27:21 +0800

    Update queue format

--- models/queue.js
+++ models/queue.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const QueueSchema = new Schema({
-    guestId: {
-        type: String,
-        required: [true, "Guest ID field required"]
-    },
     category: {
         type: Number,
         required: [true, "Category field required"]
     },
+    token: {
+        type: String, 
+        required: [true, "Token field required"]
+    }
 }, {timestamps: true });
 
 const Queue = mongoose.model('queue', QueueSchema);
