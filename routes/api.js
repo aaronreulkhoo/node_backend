42d2b345ffc85bcc392bf16058c660ce4a22adfe
Genius-wangzilin <57471983+Genius-wangzilin@users.noreply.github.com>
2020-04-25 23:08:14 +0800

    add authorization hard-coded string

--- routes/api.js
+++ routes/api.js
@@ -83,12 +83,12 @@ async function loadRainbow() {
 }
 loadRainbow();
 
+const serverKey= "BBO5e7IVtK9TeSAQ3RTYGsQOWOZ0QAe8k9jbvomydoOUEjK1lwTLIkK4J3yu";
 //check authentication of the request
 function checkAuth(req,res,next) {
     const bearerHeader = req.headers['authorization'];
     if (typeof bearerHeader !=="undefined"){
-        const bearer = bearerHeader.split(' ');
-        req.auth=bearer[1];
+        req.auth=bearerHeader;
         next()
     } else {
         res.sendStatus(401);
@@ -101,126 +101,140 @@ let ttl = 86400; // active for a day
 
 //get all agents' information
 router.get("/status", checkAuth, async (req, res, next) => {
-    Agent.find({}, function(err, agents) {
-        res.send(agents);  
-    });
+    if(req.auth===serverKey){
+        Agent.find({}, function(err, agents) {
+            res.send(agents);  
+        });
+    }else{
+        res.sendStatus(401);
+    }
+    
 });
 
-//Update Agent's Feedbacks
 router.patch("/review", checkAuth, function(req, res, next){
-    
-    try{
-        //Check if neccessary inputs are received
-        console.log('PATCH: api/review');
-        if(!req.query.rating1){
-            throw new Error("PATCH Request Needs 'Rating1' Number Parameter");
-        }
-        if(!req.query.rating2){
-            throw new Error("PATCH Request Needs 'Rating2' Number Parameter");
-        }
-        if(!req.query.rating3){
-            throw new Error("PATCH Request Needs 'Rating3' Number Parameter");
-        }
-        if(!req.query.agentId){
-            throw new Error("PATCH Request Needs 'AgentId' String Parameter");
-        }
-        if(req.query.rating1<0 || req.query.rating1>5){
-            throw new Error("'Rating1' Exceeds Threshold Value");
-        }
-        if(req.query.rating2<0 || req.query.rating2>5){
-            throw new Error("'Rating2' Exceeds Threshold Value");
-        }
-        if(req.query.rating3<0 || req.query.rating3>5){
-            throw new Error("'Rating3' Exceeds Threshold Value");
-        }
-    }catch(e){
-            return next(e);
+    console.log(req.auth);
+    if(req.auth===serverKey){
+        try{
+                //Check if neccessary inputs are received
+                console.log('PATCH: api/review');
+                if(!req.query.rating1){
+                    throw new Error("PATCH Request Needs 'Rating1' Number Parameter");
+                }
+                if(!req.query.rating2){
+                    throw new Error("PATCH Request Needs 'Rating2' Number Parameter");
+                }
+                if(!req.query.rating3){
+                    throw new Error("PATCH Request Needs 'Rating3' Number Parameter");
+                }
+                if(!req.query.agentId){
+                    throw new Error("PATCH Request Needs 'AgentId' String Parameter");
+                }
+                if(req.query.rating1<0 || req.query.rating1>5){
+                    throw new Error("'Rating1' Exceeds Threshold Value");
+                }
+                if(req.query.rating2<0 || req.query.rating2>5){
+                    throw new Error("'Rating2' Exceeds Threshold Value");
+                }
+                if(req.query.rating3<0 || req.query.rating3>5){
+                    throw new Error("'Rating3' Exceeds Threshold Value");
+                }
+            }catch(e){
+                    return next(e);
+            }
+            Agent.findOne({rainbowId: req.query.agentId}).then(function(agent){
+                if(!agent){
+                    throw new Error("Invalid Agent ID");
+                }else{
+                    //Update the average rating values
+                    var newNumberOfRating;
+                    var newAverage1;
+                    var newAverage2;
+                    var newAverage3;
+                    if(agent.numberOfRating==0){
+                        newNumberOfRating = 1;
+                        newAverage1 = req.query.rating1;
+                        newAverage2 = req.query.rating2;
+                        newAverage3 = req.query.rating3;
+                    }else{
+                        newNumberOfRating = agent.numberOfRating+1;
+                        newAverage1 = agent.averageRating1*agent.numberOfRating/newNumberOfRating+req.query.rating1/newNumberOfRating;
+                        newAverage2 = agent.averageRating2*agent.numberOfRating/newNumberOfRating+req.query.rating2/newNumberOfRating;
+                        newAverage3 = agent.averageRating3*agent.numberOfRating/newNumberOfRating+req.query.rating3/newNumberOfRating;
+                    }
+                    //If comment found, insert into feedbacks array
+                    if(!req.query.email || !req.query.comment){
+                        Agent.findByIdAndUpdate(agent._id, {
+                            $set:{'averageRating1': newAverage1, 'averageRating2': newAverage2, 'averageRating3': newAverage3, 'numberOfRating':newNumberOfRating}}).then(function(){
+                            res.send("Rating Updated Without Comment");
+                        }).catch(next);
+                    }else{
+                        Agent.findByIdAndUpdate(agent._id, {
+                            $push:{'feedbacks':{'email':req.query.email, 'comment':req.query.comment}},
+                            $set:{'averageRating1': newAverage1, 'averageRating2': newAverage2, 'averageRating3': newAverage3,'numberOfRating':newNumberOfRating}}).then(function(){
+                            res.send("Rating Updated With Comment");
+                        }).catch(next);
+                    }
+                }
+            }).catch(next);
+    }else{
+        res.sendStatus(401);
     }
-    Agent.findOne({rainbowId: req.query.agentId}).then(function(agent){
-        if(!agent){
-            throw new Error("Invalid Agent ID");
-        }else{
-            //Update the average rating values
-            var newNumberOfRating;
-            var newAverage1;
-            var newAverage2;
-            var newAverage3;
-            if(agent.numberOfRating==0){
-                newNumberOfRating = 1;
-                newAverage1 = req.query.rating1;
-                newAverage2 = req.query.rating2;
-                newAverage3 = req.query.rating3;
-            }else{
-                newNumberOfRating = agent.numberOfRating+1;
-                newAverage1 = agent.averageRating1*agent.numberOfRating/newNumberOfRating+req.query.rating1/newNumberOfRating;
-                newAverage2 = agent.averageRating2*agent.numberOfRating/newNumberOfRating+req.query.rating2/newNumberOfRating;
-                newAverage3 = agent.averageRating3*agent.numberOfRating/newNumberOfRating+req.query.rating3/newNumberOfRating;
-            }
-            //If comment found, insert into feedbacks array
-            if(!req.query.email || !req.query.comment){
-                Agent.findByIdAndUpdate(agent._id, {
-                    $set:{'averageRating1': newAverage1, 'averageRating2': newAverage2, 'averageRating3': newAverage3, 'numberOfRating':newNumberOfRating}}).then(function(){
-                    res.send("Rating Updated Without Comment");
-                }).catch(next);
-            }else{
-                Agent.findByIdAndUpdate(agent._id, {
-                    $push:{'feedbacks':{'email':req.query.email, 'comment':req.query.comment}},
-                    $set:{'averageRating1': newAverage1, 'averageRating2': newAverage2, 'averageRating3': newAverage3,'numberOfRating':newNumberOfRating}}).then(function(){
-                    res.send("Rating Updated With Comment");
-                }).catch(next);
-            }
-        }
-    
-        
-    }).catch(next);
-
+            
 });
 
-//Add New Agent
 router.post("/agents", checkAuth, function(req,res,next){
-    try{
-        console.log('POST received');
-        if(!req.query.name){
-            throw new Error("POST Request Needs 'Name' String Parameter");
+    if(req.auth===serverKey){
+        try{
+            console.log('POST received');
+            if(!req.query.name){
+                throw new Error("POST Request Needs 'Name' String Parameter");
+            }
+            if(!req.query.rainbowId){
+                throw new Error("POST Request Needs 'RainbowId' String Parameter");
+            }
+            if(!req.query.category){
+                throw new Error("POST Request Needs 'Category' Number Parameter");
+            }
+            if(req.query.category<0 || req.query.category>4){
+                throw new Error("'Category' Exceeds Threshold Value");
+            }
+        }catch(e){
+            return next(e);
         }
-        if(!req.query.rainbowId){
-            throw new Error("POST Request Needs 'RainbowId' String Parameter");
-        }
-        if(!req.query.category){
-            throw new Error("POST Request Needs 'Category' Number Parameter");
-        }
-        if(req.query.category<0 || req.query.category>4){
-            throw new Error("'Category' Exceeds Threshold Value");
-        }
-    }catch(e){
-        return next(e);
+        Agent.create({name:req.query.name, rainbowId:req.query.rainbowId, available:true, 
+        category:req.query.category, averageRating1: 0, averageRating2:0, numberOfRating:0,
+        feedbacks:new Array}).then(function(agent){
+            res.send("New Agent Is Created");
+        }).catch(next);
+    }else{
+        res.sendStatus(401);
     }
-    Agent.create({name:req.query.name, rainbowId:req.query.rainbowId, available:true, 
-    category:req.query.category, averageRating1: 0, averageRating2:0, numberOfRating:0,
-    feedbacks:new Array}).then(function(agent){
-        res.send("New Agent Is Created");
-    }).catch(next);
+    
 });
 
-//Delete One from Queue
 router.delete("/queue", checkAuth, function(req,res,next){
-    try {
-        //Check if neccessary inputs are received
-        console.log('DELETE: api/queue');
-        if (!req.query.token) {
-            throw new Error("DELETE Request Needs 'token' String Parameter");
+    if(req.auth===serverKey){
+        try {
+            //Check if neccessary inputs are received
+            console.log('DELETE: api/queue');
+            if (!req.query.token) {
+                throw new Error("DELETE Request Needs 'token' String Parameter");
+            }
+        } catch (e) {
+            return next(e);
         }
-    } catch (e) {
-        return next(e);
-    }
-    Queue.deleteOne({token:req.query.token}).then(function(response){
-        if (response.deletedCount) {
-            res.send("Queue Number Deleted")
-        } else {
-            res.send("Queue Number Not Found")
-        }
+        Queue.deleteOne({token:req.query.token}).then(function(response){
+            if (response.deletedCount) {
+                res.send("Queue Number Deleted")
+            } else {
+                res.send("Queue Number Not Found")
+            }
 
-    });
+        });        
+    }else{
+        res.sendStatus(401);
+    }
+    
 });
 
 module.exports = router;
